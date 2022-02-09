0d89668c72e69c972d4f7fa22b150a37f5dce252
CDKail <892020022@qq.com>
2022-02-09 10:35:49 +0800

    fix: 增加了console

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -81,6 +81,8 @@
     },
   });
 
+  console.log(123);
+
   const globSetting = useGlobSetting();
   const { prefixCls } = useDesign('login');
   const { t } = useI18n();
@@ -99,7 +101,7 @@
       background-color: @dark-bg;
 
       &::before {
-        background-image: url(/@/assets/svg/login-bg-dark.svg);
+        background-image: url('/@/assets/svg/login-bg-dark.svg');
       }
 
       .ant-input,
@@ -145,7 +147,7 @@
       width: 100%;
       height: 100%;
       margin-left: -48%;
-      background-image: url(/@/assets/svg/login-bg.svg);
+      background-image: url('/@/assets/svg/login-bg.svg');
       background-position: 100%;
       background-repeat: no-repeat;
       background-size: auto 100%;
